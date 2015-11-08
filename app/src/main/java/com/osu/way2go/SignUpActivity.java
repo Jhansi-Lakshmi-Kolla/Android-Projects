e186866f8340014a072e8c08d44250df27d964ac
hiiamgovind <hiiamgovind@gmail.com>
2015-11-07 22:48:36 -0500

    parsesignup

--- app/src/main/java/com/osu/way2go/SignUpActivity.java
+++ app/src/main/java/com/osu/way2go/SignUpActivity.java
@@ -1,18 +1,23 @@
+
 package com.osu.way2go;
 
 import android.app.AlertDialog;
 import android.content.Context;
 import android.content.DialogInterface;
+import android.net.ParseException;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
 import com.osu.way2go.db.MySQLiteDBUtility;
+import com.parse.ParseUser;
+import com.parse.SignUpCallback;
 
 public class SignUpActivity extends AppCompatActivity {
     private static final String TAG = "SignUpActivity";
@@ -43,7 +48,34 @@ public class SignUpActivity extends AppCompatActivity {
                 String password = passwordBox.getText().toString();
                 String confirmPassword = confirmPasswordBox.getText().toString();
                 if(isPasswordGood(password, confirmPassword)){
-                    registerUser(firstname, lastname,emailID,password);
+                    registerUser(firstname, lastname, emailID, password);
+                    //add in parse
+                    ParseUser user = new ParseUser();
+                    user.setUsername(emailID);
+                    user.setPassword(password);
+                    user.setEmail("emailID");
+
+                    // other fields can be set just like with ParseObject
+                    user.put("FName", firstname);
+                    user.put("LName", lastname);
+                    user.signUpInBackground(new SignUpCallback() {
+                        public void done(com.parse.ParseException e) {
+                            if(e==null)
+                            {Log.i(TAG, "Put to parse");}
+                            else
+                            {Log.i(TAG, "Not put in parse");}
+                        }
+
+//                        public void done(ParseException e) {
+//                            if(e==null)
+//                            {Log.i(TAG, "Sexy");}
+//                            else
+//                            {Log.i(TAG, "Not Sexy");}
+//
+//                        }
+
+                    });
+
                 }
             }
         });
